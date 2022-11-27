69088d820c5c73a091d2ab1b0359e18c733a10dd
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2022-11-26 21:12:23 -0300

    create project-input class

--- source/app.ts
+++ source/app.ts
@@ -1,9 +1,23 @@
-const titleEl = document.getElementById('title') as HTMLInputElement;
-const descriptionEl = document.getElementById('description') as HTMLInputElement;
-const peopleEl = document.getElementById('people') as HTMLInputElement;
-const isvalidEl = document.querySelectorAll('.is-valid') as NodeList;
-const ulEl = document.querySelector('ul') as HTMLElement;
-const check = document.querySelector('#check') as HTMLInputElement;
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.attach();
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+
+    }
+}
 
 class Project {
     title: string;
@@ -19,39 +33,50 @@ class Project {
     }
 }
 
-const projectForm = document.querySelector('form')!;
-projectForm.addEventListener('submit', event => {
+const prjInput = new ProjectInput();
+const titleEl = prjInput.element.querySelector('#title') as HTMLInputElement;
+const descriptionEl = prjInput.element.querySelector('#description') as HTMLInputElement;
+const peopleEl = prjInput.element.querySelector('#people') as HTMLInputElement;
+const isvalidEl = prjInput.element.querySelector('.is-valid') as unknown as NodeList;
+const ulEl = prjInput.element.querySelector('ul') as HTMLElement;
+const check = prjInput.element.querySelector('#check') as HTMLInputElement;
+
+prjInput.element.addEventListener('submit', event => {
     event.preventDefault();
     const title = titleEl.value;
     const description = descriptionEl.value;
     const people = +peopleEl.value;
     const active = check.checked;
 
-    if(!title && !description && !people){
-        isvalidEl.forEach((el: any) => {
-            el.textContent = 'Please provide a valid input value!!!'
-        });
-        return;
-    }
+    // if(!title && !description && !people){
+    //     isvalidEl.forEach((el: any) => {
+    //         el.textContent = 'Please provide a valid input value!!!'
+    //     });
+    //     return;
+    // }
     const newCourse = new Project(title, description, people, active);
 
-    Object.values(newCourse).forEach((el: any) => {
-        const li = document.createElement('li');
-        li.innerHTML = el;
-        ulEl.appendChild(li);
-    })
-    localStorage.setItem('items', JSON.stringify(newCourse));
-    titleEl.value = ''
-    descriptionEl.value = ''
-    peopleEl.value = ''
+    // Object.values(newCourse).forEach((el: any) => {
+    //     const li = prjInput.element.querySelector('li')! as HTMLLIElement;
+    //     const ht = prjInput.element.querySelector('#single-project') as HTMLTemplateElement;
+    //     li.innerHTML = el;
+    //     ht.appendChild(li);
+    // })
+    // localStorage.setItem('items', JSON.stringify(newCourse));
+    // titleEl.value = ''
+    // descriptionEl.value = ''
+    // peopleEl.value = ''
     
 })
 
-let items = JSON.parse(localStorage.getItem('items')! as string);
-if(items){
-    Object.values(items).forEach((el: any) => {
-        const li = document.createElement('li');
-        li.innerHTML = el;
-        ulEl.appendChild(li);
-    })
-}
+// let items = JSON.parse(localStorage.getItem('items')! as string);
+// if(items){
+//     Object.values(items).forEach((el: any) => {
+//         const li = document.createElement('li');
+//         li.innerHTML = el;
+//         ulEl.appendChild(li);
+//     })
+// }
+
+
+
