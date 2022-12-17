fad091dbd37bbb66b105254a50e75a31d728349b
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2022-12-17 18:35:50 -0300

    getting single project by id

--- server/server/route/project.py
+++ server/server/route/project.py
@@ -4,6 +4,7 @@ import pymongo
 import json
 import datetime
 from bson import json_util
+from bson.objectid import ObjectId
 from flask import Response, request, abort, jsonify
 from flask_restful import Resource
 
@@ -22,7 +23,33 @@ class Project(Resource):
 
     @staticmethod
     def get():
-        pass
+        data = db.project_active.find()
+        return json.loads(json_util.dumps(data))
+
+
+    @staticmethod
+    def post():
+        project_info = request.json
+        title = project_info.get('title')
+        description = project_info.get('description')
+        people = project_info.get('people')
+        status = project_info.get('status')
+        new_project = AddProject(title, description, people, status)
+        db.project_active.insert_one({
+            "title": new_project.title, 
+            "description": new_project.description, 
+            "people": new_project.people,
+            "status": new_project.status,
+            "created_at": new_project.created_at
+            })
+        return Response(response=json.dumps('Project added sucessfully!'), status=201)
+
+
+class SingleProject(Resource):
+
+    def get(self, project_id:str):
+        single_proj = db.project_active.find_one({"_id": ObjectId(project_id)})
+        return json.loads(json_util.dumps(single_proj))
 
 
 class AddProject():
