d3169263dad62b0d909239a3259a1d219b414934
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2022-12-17 18:07:50 -0300

    Add files via upload

--- server/api.py
+++ server/api.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
-from server.route.project import Project
+from server.route.project import Project, SingleProject
 
 APP = Flask(__name__)
 API = Api(APP)
 PORT = 8080
 
+
 API.add_resource(Project, '/project')
+API.add_resource(SingleProject, '/single_project/<string:project_id>')
 
 
 if __name__ == '__main__':
