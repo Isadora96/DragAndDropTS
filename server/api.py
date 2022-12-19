c65524dd7666742dd26f68af92c24b3f685c2deb
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2022-12-19 19:50:46 -0300

    create finished project route

--- server/api.py
+++ server/api.py
@@ -3,6 +3,7 @@ from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 from server.route.active_project.active_project import ActiveProject
 from server.route.active_project.single_active_project import SingleActiveProject
+from server.route.finished_project.finished_project import FinishedProject
 
 APP = Flask(__name__)
 API = Api(APP)
@@ -11,6 +12,7 @@ PORT = 8080
 
 API.add_resource(ActiveProject, '/active_project')
 API.add_resource(SingleActiveProject, '/single_active_project/<string:project_id>')
+API.add_resource(FinishedProject, '/finished_project')
 
 
 if __name__ == '__main__':
