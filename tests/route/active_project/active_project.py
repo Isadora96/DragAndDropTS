884406872576d9c627a9fff4e7b2d457f21eee93
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2022-12-25 18:27:06 -0300

    improving unittest for bad requests

--- tests/route/active_project/active_project.py
+++ tests/route/active_project/active_project.py
@@ -3,7 +3,7 @@ from unittest.mock import patch
 from werkzeug.exceptions import BadRequest
 
 from server.route.active_project.active_project import ActiveProject
-from server.db.db import db
+
 
 class ActiveProjectTestCase(unittest.TestCase):
 
@@ -17,27 +17,59 @@ class ActiveProjectTestCase(unittest.TestCase):
         self.assertEqual(data, {})
 
     @patch('server.route.active_project.active_project.request')
-    def test_post_method_returns_status__201(self, request_mock):
+    @patch('server.route.active_project.active_project.db')
+    def test_post_method_returns_status__201(self, db_mock, request_mock):
 
         request_mock.json = {
             "title": "title",
             "description": "description",
             "people": 1,
-            "status": "status"
         }
         
         response = ActiveProject.post()
 
         self.assertEqual(response.status, '201 CREATED')
+        self.assertEqual(response.response, [b'"Project added sucessfully!"'])
 
     @patch('server.route.active_project.active_project.request')
-    def test_post_method_missing_data_status__400(self, request_mock):
+    @patch('server.route.active_project.active_project.db')
+    def test_post_method_missing_data_status__400(self, db_mock, request_mock):
 
         request_mock.json = {
             "title": "",
             "description": "",
             "people": 0,
-            "status": ""
         }        
+
         with self.assertRaises(BadRequest):
             ActiveProject.post()
+            self.assertEqual(ActiveProject.post().response.response, 'Project title, description or people missing!')
+
+    @patch('server.route.active_project.active_project.request')
+    @patch('server.route.active_project.active_project.db')
+    def test_post_method_people_less_than_0_status__400(self, db_mock, request_mock):
+
+        request_mock.json = {
+            "title": "title",
+            "description": "description",
+            "people": 0,
+        }        
+
+        with self.assertRaises(BadRequest):
+            ActiveProject.post()        
+            self.assertEqual(ActiveProject.post().response.response, 'Total people must be at least 1!')
+
+    @patch('server.route.active_project.active_project.request')
+    @patch('server.route.active_project.active_project.db')
+    def test_post_method_bad_json_data_status__400(self, db_mock, request_mock):
+
+        request_mock.json = {
+            "title": "title",
+            "description": "description",
+            "people": 5,
+            "other_data": ""
+        }        
+
+        with self.assertRaises(BadRequest):
+            ActiveProject.post()        
+            self.assertEqual(ActiveProject.post().response.response, 'Please, provide only project title, description and people!')
