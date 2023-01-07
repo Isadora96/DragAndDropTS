b0f21dd77fe59d59a3406fef82267e6ade10fb8a
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-01-07 16:36:31 -0300

    Add files via upload

--- server/db/db.py
+++ server/db/db.py
@@ -5,7 +5,7 @@ import os
 from urllib.parse import quote_plus
 
 username = quote_plus(str(os.environ.get('username')))
-password = quote_plus(str(os.environ.get('username')))
+password = quote_plus(str(os.environ.get('password')))
 
 myclient = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.xrhek5n.mongodb.net/?retryWrites=true&w=majority", 
                                 tlsCAFile=certifi.where())
