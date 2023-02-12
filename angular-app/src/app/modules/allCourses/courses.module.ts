56bad6e3067661420999c76429ea27e2ebb9ae59
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-12 16:30:11 -0300

    Add files via upload

--- angular-app/src/app/modules/allCourses/courses.module.ts
+++ angular-app/src/app/modules/allCourses/courses.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from "@angular/core";
 import { AllCoursesRoutingModule } from "./courses-routing.module";
 import { AllCoursesComponent } from "./components/courses.component";
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
     declarations: [
@@ -9,7 +12,9 @@ import { CommonModule } from '@angular/common';
     ],
     imports: [
         AllCoursesRoutingModule,
-        CommonModule
+        CommonModule,
+        MatCardModule,
+        MatIconModule
     ],
     exports: [
         AllCoursesComponent
