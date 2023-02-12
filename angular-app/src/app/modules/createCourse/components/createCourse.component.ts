a08a98646ef01e5fd60163edd0c9f28f7405d6d5
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-12 16:13:16 -0300

    adding form

--- angular-app/src/app/modules/createCourse/components/createCourse.component.ts
+++ angular-app/src/app/modules/createCourse/components/createCourse.component.ts
@@ -1,17 +1,33 @@
 import { Component, HostBinding, HostListener } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-
+import { CoursesService } from 'src/app/courses.service';
+import { Course } from '../../shared/models/course.model';
 
 
 @Component({
-    selector: 'app-course',
+    selector: 'app-create-course',
     templateUrl: './createCourse.component.html',
     styleUrls: ['./createCourse.component.css']
 })
 
 export class CreateCourseComponent {
 
-    constructor() {
-        
+    title: string = '';
+    description: string = '';
+    people!: number;
+
+    constructor(private coursesService: CoursesService) { }
+
+    submitted = false;
+
+    onSubmit() {
+        this.submitted = true;
     }
+
+
+  newCourse() {
+    const course = new Course(this.title, this.description, this.people);
+    this.coursesService.postCourse(course).subscribe(res => {
+      console.log(res);
+    })
+  }
 }
