1930f75220df4312bf0ebb1eaf2cab77e9281025
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-12 13:46:37 -0300

    add favorite option

--- angular-app/src/app/modules/allCourses/components/courses.component.ts
+++ angular-app/src/app/modules/allCourses/components/courses.component.ts
@@ -10,6 +10,7 @@ import { CoursesService } from 'src/app/courses.service';
 export class AllCoursesComponent {
 
     courses: any = [];
+    fav: boolean = false;
 
     constructor(private coursesService: CoursesService) {
 
@@ -20,5 +21,16 @@ export class AllCoursesComponent {
             this.courses = data
         });
     }
+
+    favorite($event: Event) {
+        //$event.preventDefault();
+        const favoriteIcons = document.querySelectorAll(".favorite-icon-course");
+        favoriteIcons.forEach(favoriteIcon => {
+            favoriteIcon.addEventListener('click', () => {
+                favoriteIcon.classList.toggle('mat-icon-red')
+            })
+        })
+    }
+
 
 }
