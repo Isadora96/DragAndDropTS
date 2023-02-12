5d347a349b6a73d2a0bc90b5610d07d84638a923
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-12 16:10:16 -0300

    adding export/import tooltip component

--- angular-app/src/app/modules/shared/shared.module.ts
+++ angular-app/src/app/modules/shared/shared.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { TooltipComponent } from './components/tooltip/tooltip.component';
+import { Course } from './models/course.model';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
     declarations: [
         HeaderComponent,
+        TooltipComponent,        
   ],
   imports: [
     MatToolbarModule,
     MatIconModule,
+    MatTooltipModule,
+    
   ],
   exports: [
-    HeaderComponent //to export needs to be declarated
+    HeaderComponent, //to export needs to be declarated
+    TooltipComponent,
+    
   ],
 })
 
