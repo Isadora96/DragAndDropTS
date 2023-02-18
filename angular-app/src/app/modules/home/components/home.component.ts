7c09a9a4fb6f0f276b95daccd597de582c999818
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-18 17:50:47 -0300

    add checkbox column

--- angular-app/src/app/modules/home/components/home.component.ts
+++ angular-app/src/app/modules/home/components/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { CoursesService } from 'src/app/courses.service';
+import {SelectionModel} from '@angular/cdk/collections';
+import { Course } from '../../shared/models/course.model';
 
 @Component({
     selector: 'app-home',
@@ -8,19 +10,66 @@ import { CoursesService } from 'src/app/courses.service';
 })
 
 export class HomeComponent  {
-    displayedColumns: string[] = ['title', 'description', 'people', 'status', 'created_at']; 
-    courses: any = [];
-  
-    resultsLength = 0;
-    isLoadingResults = true;
-    isRateLimitReached = false;
+    displayedColumns: string[] = ['select', 'title', 'description', 'people', 'status', 'created_at']; 
+    courses: any = [];  
+    selection = new SelectionModel<Course>(true, []);
+    toggled: Boolean = false;
     
     constructor(private coursesService: CoursesService) { }
 
+
     ngOnInit() {
+        this.loadResults();
+    };
+
+      /** Whether the number of selected elements matches the total number of rows. */
+    isAllSelected() {
+        const numSelected = this.selection.selected.length;
+        const numRows = this.courses.length;
+        return numSelected === numRows;
+    }
+
+    /** Selects all rows if they are not all selected; otherwise clear selection. */
+    toggleAllRows() {
+        if (this.isAllSelected()) {
+            this.selection.clear();
+            return;
+        }
+        this.selection.select(...this.courses);
+    }
+
+    /** The label for the checkbox on the passed row */
+    checkboxLabel(row?: Course) {
+        if (!row) {
+            return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+        }
+        return `${this.selection.isSelected(row) ? 'deselect' : 'select'}}`;
+     }
+
+    loadResults() {
         this.coursesService.getCourses().subscribe(data => {
             this.courses = data
-        });
+        })
+    }
+
+    onClick($event: { stopPropagation: () => void; }) {
+        $event.stopPropagation();
+        this.toggled = true;
+        const modal = document.querySelector('#modal');
+        if(this.toggled) {
+            modal?.setAttribute('class', 'display-block')
+        } else {
+            modal?.setAttribute('class', 'display-none')
+        }
+
+    }
+
+    onClose(event: Event) {
+        event.stopPropagation();
+        this.toggled = false;
+        const modal = document.querySelector('#modal');
+        modal?.setAttribute('class', 'display-none')
+        this.selection.clear();
     }
 }
   
