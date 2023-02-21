44f35baea547f47850cc94324a7f0c07886eb51b
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-21 20:09:50 -0300

    add routes

--- angular-app/src/app/courses.service.ts
+++ angular-app/src/app/courses.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
-import { Course } from "./modules/shared/models/course.model";
+import { Course, CourseUpdate } from "./modules/shared/models/course.model";
 // import { Observable, throwError } from 'rxjs';
 import { map } from 'rxjs/operators';
 
@@ -18,6 +18,10 @@ export class CoursesService {
         return this.http.get(`${this.coursesUrl}/active_project`);
     }
 
+    getSingleCourse(courseId: string) {
+        return this.http.get(`${this.coursesUrl}/single_active_project/${courseId}`);
+    }
+
     postCourse(course: Course) {
         return this.http.post(`${this.coursesUrl}/active_project`, course , {	observe: 'body' }).pipe(map((result: any) => result));
     }
@@ -28,5 +32,9 @@ export class CoursesService {
 
     deleteAllCourses() {
         return this.http.delete(`${this.coursesUrl}/active_project`).pipe(map((result: any) => result));
+    }
+
+    updateCourse(course: CourseUpdate) {
+        return this.http.put(`${this.coursesUrl}/active_project`, course).pipe(map((result: any) => result));
     }
 }
