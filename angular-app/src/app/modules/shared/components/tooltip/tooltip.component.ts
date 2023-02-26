387c90d5a7d8454ff54922709c0c55eaaf2b83d7
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-02-26 15:38:16 -0300

    add badge tooltip

--- angular-app/src/app/modules/shared/components/tooltip/tooltip.component.ts
+++ angular-app/src/app/modules/shared/components/tooltip/tooltip.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
+import {Component, Input } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {TooltipPosition} from '@angular/material/tooltip';
+import { FavoritesService } from '../../services/favorites.service';
 
 @Component({
   selector: 'app-tooltip',
@@ -11,4 +12,14 @@ import {TooltipPosition} from '@angular/material/tooltip';
 export class TooltipComponent {
   positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
   position = new FormControl(this.positionOptions[0]);
+
+  favNumber: number | undefined;
+
+
+  constructor(private favoritesService: FavoritesService) {}
+
+  ngOnInit(): void {
+    this.favoritesService.getData().subscribe((data) => (this.favNumber = data));
+  }
+
 }
