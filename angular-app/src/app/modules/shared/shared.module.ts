60279cdcba280be7a02f32cc9ed00024d00868c6
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-01 19:15:44 -0300

    Add files via upload

--- angular-app/src/app/modules/shared/shared.module.ts
+++ angular-app/src/app/modules/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { TooltipComponent } from './components/tooltip/tooltip.component';
+import { FooterComponent } from './components/footer/footer.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
@@ -9,7 +10,8 @@ import {MatBadgeModule} from '@angular/material/badge';
 @NgModule({
     declarations: [
         HeaderComponent,
-        TooltipComponent,        
+        TooltipComponent,    
+        FooterComponent    
   ],
   imports: [
     MatToolbarModule,
@@ -21,7 +23,7 @@ import {MatBadgeModule} from '@angular/material/badge';
   exports: [
     HeaderComponent, //to export needs to be declarated
     TooltipComponent,
-    
+    FooterComponent
   ],
 })
 
