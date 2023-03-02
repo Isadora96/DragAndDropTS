1d47184771f9d10fd5a60bf97cbfb2c24eb92d59
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-02 14:59:47 -0300

    Add files via upload

--- angular-app/src/app/modules/shared/services/courses.service.ts
+++ angular-app/src/app/modules/shared/services/courses.service.ts
@@ -15,29 +15,33 @@ export class CoursesService {
     constructor(private http: HttpClient) {}
 
     getCourses() {
-        return this.http.get(`${this.coursesUrl}/active_project`);
+        return this.http.get(`${this.coursesUrl}/courses`);
     }
 
     getSingleCourse(courseId: string) {
-        return this.http.get(`${this.coursesUrl}/active_project/${courseId}`);
+        return this.http.get(`${this.coursesUrl}/single_course/${courseId}`);
     }
 
     postCourse(course: Course) {
-        return this.http.post(`${this.coursesUrl}/active_project`, course , {	observe: 'body' }).pipe(map((result: any) => result));
+        return this.http.post(`${this.coursesUrl}/courses`, course , {	observe: 'body' }).pipe(map((result: any) => result));
     }
 
-    deleteCourse(courseId: string) {
-        return this.http.delete(`${this.coursesUrl}/active_project/${courseId}`).pipe(map((result: any) => result));
+    deleteSingleCourse(courseId: string) {
+        return this.http.delete(`${this.coursesUrl}/single_course/${courseId}`).pipe(map((result: any) => result));
     }
 
-    deleteAllCourses() {
-        return this.http.delete(`${this.coursesUrl}/active_project`).pipe(map((result: any) => result));
+    deleteAllActiveCourses() {
+        return this.http.delete(`${this.coursesUrl}/courses`).pipe(map((result: any) => result));
+    }
+
+    deleteAllFinshiedCourses() {
+        return this.http.delete(`${this.coursesUrl}/finished_courses`).pipe(map((result: any) => result));
     }
 
     updateCourse(course: CourseUpdate) {
         const headers = new HttpHeaders({
             'Content-Type': 'application/json'
           });
-        return this.http.put(`${this.coursesUrl}/active_project`, course, { headers }).pipe(map((result: any) => result));
+        return this.http.put(`${this.coursesUrl}/courses`, course, { headers }).pipe(map((result: any) => result));
     }
 }
