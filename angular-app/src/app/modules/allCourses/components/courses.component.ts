d1348e3b5992d1379535dbc518dea1c4d6c9beea
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-04 18:38:37 -0300

    add avatar image

--- angular-app/src/app/modules/allCourses/components/courses.component.ts
+++ angular-app/src/app/modules/allCourses/components/courses.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, OnInit } from '@angular/core';
 import { CoursesService } from 'src/app/modules/shared/services/courses.service';
 import { FavoritesService } from '../../shared/services/favorites.service';
 
@@ -13,7 +13,37 @@ export class AllCoursesComponent implements OnInit {
     courses: any = [];
     favorites: any = [];
 
-    constructor(private coursesService: CoursesService, private favoritesService: FavoritesService) {}
+    constructor(private coursesService: CoursesService, private favoritesService: FavoritesService, private elementRef: ElementRef) {}
+    
+    ngAfterViewChecked(): void {
+        this.setAvatarImage()
+    }
+
+    setAvatarImage() {
+        const avatar = this.elementRef.nativeElement.querySelectorAll('.avatar');
+
+        const authors = this.courses.map((course: { author: any; }) => course.author[0]);
+
+        avatar.forEach((element: { style: { backgroundImage: string; }; }, index: number) => {
+            const letters = authors[index].split(' ')[0][0] + authors[index].split(' ')[1][0]
+            const canvas = document.createElement('canvas') as HTMLCanvasElement;
+
+            canvas.height = 200;
+            canvas.width = 200;
+    
+            const ctx = canvas.getContext('2d')!;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.fillStyle = '#D3D3D3';
+            ctx.font = 'bold 7rem sans-serif';
+            ctx.textAlign="center";
+            ctx.textBaseline="middle";
+
+
+            ctx.fillText(letters.toUpperCase(), 100, 110); 
+            const backgroundImageUrl = canvas.toDataURL();
+            element.style.backgroundImage = `url(${backgroundImageUrl})`;
+        })
+    }
 
     ngOnInit() {
         this.favorites = localStorage.getItem('favorites.courses') ? JSON.parse(localStorage.getItem('favorites.courses')!) : []
