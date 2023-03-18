c992102f2f451139a77f37c124696bdea22db6de
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-18 16:58:15 -0300

    Delete server/classes directory

--- server/classes/get_date.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-class GetDate():
-
-    get_date = datetime.datetime.now()
-
-    def date(self):
-        return f'{str(self.get_date.year)}-{str(self.get_date.month)}-{str(self.get_date.day)}'
