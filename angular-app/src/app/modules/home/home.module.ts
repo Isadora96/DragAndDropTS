55fe5440cedea1e71182d9005adffb68c711382c
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-20 19:03:24 -0300

    Add files via upload

--- angular-app/src/app/modules/home/home.module.ts
+++ angular-app/src/app/modules/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home.component';
+import { LoadingModule } from '../shared/loading/loading.module';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { MatTableModule } from '@angular/material/table';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatTabsModule} from '@angular/material/tabs';
+
 @NgModule({
     declarations: [
         HomeComponent
@@ -14,7 +16,8 @@ import {MatTabsModule} from '@angular/material/tabs';
         MatTableModule,
         MatCheckboxModule,
         MatTabsModule,
-        CommonModule
+        CommonModule,
+        LoadingModule
     ],
     exports: [
         HomeComponent
