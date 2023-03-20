2a3ecf1a35db988d887a55aed912845912f02f0e
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-20 19:28:15 -0300

    Update db.py

--- server/api/db/db.py
+++ server/api/db/db.py
@@ -6,8 +6,9 @@ from urllib.parse import quote_plus
 
 username = quote_plus(str(os.environ.get('username')))
 password = quote_plus(str(os.environ.get('password')))
+cluster = quote_plus(str(os.environ.get('cluster')))
 
-myclient = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.xrhek5n.mongodb.net/?retryWrites=true&w=majority", 
+myclient = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@{cluster}.mongodb.net/?retryWrites=true&w=majority", 
                                 tlsCAFile=certifi.where())
 
 try:
