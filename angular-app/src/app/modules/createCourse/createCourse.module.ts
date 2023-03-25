bd7f31ca36d61d5fd9efc54390d7e3852403d396
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-03-25 17:44:59 -0300

    remove window alert and add snacker

--- angular-app/src/app/modules/createCourse/createCourse.module.ts
+++ angular-app/src/app/modules/createCourse/createCourse.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { LoadingModule } from '../shared/loading/loading.module';
 import { CreateCourseComponent } from './components/createCourse.component';
 import { CreateCourseRoutingModule } from './createCourse-routing.module';
 import {MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
-
+import { SnackBarModule } from '../shared/snackbar/snackbar.module';
 @NgModule({
     declarations: [
         CreateCourseComponent
@@ -15,7 +16,9 @@ import {MatSelectModule} from '@angular/material/select';
         MatInputModule,
         FormsModule,
         MatSelectModule,
-        CommonModule
+        CommonModule,
+        LoadingModule,
+        SnackBarModule
     ],
     exports: [
         CreateCourseComponent
