44d1cfd0400fd18586ba3838a231415c15af539f
Isadora Lopes <80006401+Isadora96@users.noreply.github.com>
2023-04-01 18:22:28 -0300

    Delete angular-app/src/environments directory

--- angular-app/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    serverApi: "https://python-production-7f15.up.railway.app"
-};
--- angular-app/src/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: false,
-    serverApi: "http://localhost:8080"
-};
